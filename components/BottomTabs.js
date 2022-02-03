d722c55b4c1516ada59c7bf0a55646e4874b65e6
HakimMaz <maz.hakim93@gmail.com>
2022-02-03 15:53:32 +0100

    adding icons to bottomTabs

--- components/BottomTabs.js
+++ components/BottomTabs.js
@@ -1,29 +1,33 @@
-import { View, Text } from 'react-native';
-import React from 'react';
-
+import { View, Text, TouchableOpacity } from "react-native";
+import React from "react";
+import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
 export default function BottomTabs() {
   return (
-    <View style={{
-        flexDirection:'row',
-        justifyContent:'space-between',
-        marginHorizontal:30,
-        marginTop:10}}>
-      <View>
-          <Text>1</Text>
-          <Text>1</Text>
-      </View>
-      <View>
-          <Text>2</Text>
-          <Text>2</Text>
-      </View>
-      <View>
-          <Text>3</Text>
-          <Text>3</Text>
-      </View>
-      <View>
-          <Text>4</Text>
-          <Text>4</Text>
-      </View>
+    <View
+      style={{
+        flexDirection: "row",
+        justifyContent: "space-between",
+        marginHorizontal: 30,
+        marginTop: 10,
+      }}
+    >
+      <Icon name="home" title="Home" />
+      <Icon name="search" title="Browse" />
+      <Icon name="shopping-bag" title="Grosery" />
+      <Icon name="receipt" title="Orders" />
+      <Icon name="user" title="Account" />
     </View>
   );
 }
+
+const Icon = ({ name, title }) => (
+  <TouchableOpacity>
+    <FontAwesome5
+      name={name}
+      size={25}
+      style={{ marginBottom: 3, alignSelf: "center" }}
+    />
+
+    <Text>{title}</Text>
+  </TouchableOpacity>
+);
