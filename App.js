002137cf5cc5fa00332f469a2315f7a3bc1722da
HakimMaz <maz.hakim93@gmail.com>
2022-02-04 00:32:40 +0100

    creating seconde screen restaurant Detail

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { View, Text } from "react-native";
 import Home from "./screens/Home";
+import RestaurantDetail from "./screens/RestaurantDetail";
 
 export default function App() {
   return (
     <View>
-      <Home/>
+      {/* <Home/> */}
+      <RestaurantDetail/>
     </View>
   );
 }
